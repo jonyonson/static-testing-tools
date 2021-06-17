ee1674e8e411c37c86e30460cbf6ccfb7590bd58
Jonathan Taylor <jonathan.taylor@daveramsey.com>
2021-06-17 15:13:43 -0500

    testing

--- src/typescript-example.js
+++ src/typescript-example.js
@@ -1,9 +1,12 @@
 function add(a, b) {
-    return a + b;
+  return a + b
 }
 function getFullName(user) {
-    var _a = user.name, first = _a.first, middle = _a.middle, last = _a.last;
-    return [first, middle, last].filter(Boolean).join(' ');
+  var _a = user.name,
+    first = _a.first,
+    middle = _a.middle,
+    last = _a.last
+  return [first, middle, last].filter(Boolean).join(' ')
 }
-add(1, 'two');
-getFullName({ name: { first: 'Joe', middle: 'Bud', last: 'Matthews' } });
+add(1, 'two')
+getFullName({name: {first: 'Joe', middle: 'Bud', last: 'Matthews'}})
